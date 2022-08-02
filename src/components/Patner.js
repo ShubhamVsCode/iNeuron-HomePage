17e2692ae0d1511cd529263875d24d3084f9819d
Shubham <99742546+ShubhamVsCode@users.noreply.github.com>
2022-08-02 20:31:07 +0530

    Update Patner.js

--- src/components/Patner.js
+++ src/components/Patner.js
@@ -8,7 +8,7 @@ const Patner = () => {
   return (
     <div className="flex items-center justify-between h-64 p-12 px-20 mx-auto my-10 shadow-lg sm:flex-col sm:p-4 mt-52 shadow-blue-200 rounded-3xl partner-background sm:h-40 sm:m-4 max-w-7xl">
       <div>
-        <p className="text-5xl font-bold text-center text-white drop-shadow-md sm:font-normal sm:text-3xl w-68">
+        <p className="text-5xl font-bold sm:text-center text-white drop-shadow-md sm:font-normal sm:text-3xl w-68">
           Our Partnered <br /> Institutions
           <img src={bank} className="inline-block w-10 ml-3 sm:hidden" alt="" />
         </p>
